07fb370ff75807864aa4928dd0a879b2f08af361
YWang <yuqiiwang@outlook.com>
2025-06-30 23:30:03 +0800

    add protection for WENO interpolations

--- Src/AmrCore/AMReX_Interpolater.cpp
+++ Src/AmrCore/AMReX_Interpolater.cpp
@@ -7,6 +7,7 @@
 #include <AMReX_MFInterp_C.H>
 
 #include <climits>
+#include <IndexMacro.H>
 
 namespace amrex {
 
@@ -1670,6 +1671,16 @@ CellWENO::interp (const FArrayBox& crse,
     AMREX_HOST_DEVICE_PARALLEL_FOR_4D_FLAG(runon, bz, ncomp, i, j, k, n,
     {
         central_weno_interp_z(i,j,k,n,tmpzarr,crsearr,ratio);
+#ifdef USE_FUEGO_EOS
+        // facilitate robustness
+        Real rhomin = 1e-10;
+        Real Tmin = 1e-10;
+        tmpzarr(i,j,k,MRHO) = amrex::max(tmpzarr(i,j,k,MRHO), rhomin);
+        for (int n = 0; n < NSP; ++n) {
+            tmpzarr(i,j,k,n) = amrex::max(tmpzarr(i,j,k,n),0.0);  
+        }
+        tmpzarr(i,j,k,MT) = amrex::max(tmpzarr(i,j,k,MT), Tmin);
+#endif
     });
 #endif
 
@@ -1691,6 +1702,16 @@ CellWENO::interp (const FArrayBox& crse,
     AMREX_HOST_DEVICE_PARALLEL_FOR_4D_FLAG(runon, by, ncomp, i, j, k, n,
     {
         central_weno_interp_y(i,j,k,n,tmpyarr,srcarr,ratio);
+#ifdef USE_FUEGO_EOS
+        // facilitate robustness
+        Real rhomin = 1e-10;
+        Real Tmin = 1e-10;
+        tmpyarr(i,j,k,MRHO) = amrex::max(tmpyarr(i,j,k,MRHO), rhomin);
+        for (int n = 0; n < NSP; ++n) {
+            tmpyarr(i,j,k,n) = amrex::max(tmpyarr(i,j,k,n),0.0);  
+        }
+        tmpyarr(i,j,k,MT) = amrex::max(tmpyarr(i,j,k,MT), Tmin);
+#endif
     });
 #endif
 
@@ -1703,6 +1724,16 @@ CellWENO::interp (const FArrayBox& crse,
                                            i, j, k, n,
     {
         central_weno_interp_x(i,j,k,n,finearr,srcarr,ratio);
+#ifdef USE_FUEGO_EOS
+        // facilitate robustness
+        Real rhomin = 1e-10;
+        Real Tmin = 1e-10;
+        finearr(i,j,k,MRHO) = amrex::max(finearr(i,j,k,MRHO), rhomin);
+        for (int n = 0; n < NSP; ++n) {
+            finearr(i,j,k,n) = amrex::max(finearr(i,j,k,n),0.0);  
+        }
+        finearr(i,j,k,MT) = amrex::max(finearr(i,j,k,MT), Tmin);
+#endif
    });
 }
 
@@ -1746,6 +1777,16 @@ CellWENO::restrict (const FArrayBox& fine,
     AMREX_HOST_DEVICE_PARALLEL_FOR_4D_FLAG(runon, bz, ncomp, i, j, k, n,
     {
         weno_restrict_z(i,j,k,n,tmpzarr,finearr,ratio);
+#ifdef USE_FUEGO_EOS
+        // facilitate robustness
+        Real rhomin = 1e-10;
+        Real Tmin = 1e-10;
+        tmpzarr(i,j,k,MRHO) = amrex::max(tmpzarr(i,j,k,MRHO), rhomin);
+        for (int n = 0; n < NSP; ++n) {
+            tmpzarr(i,j,k,n) = amrex::max(tmpzarr(i,j,k,n),0.0);  
+        }
+        tmpzarr(i,j,k,MT) = amrex::max(tmpzarr(i,j,k,MT), Tmin);
+#endif
     });
 #endif
 
@@ -1765,6 +1806,16 @@ CellWENO::restrict (const FArrayBox& fine,
     AMREX_HOST_DEVICE_PARALLEL_FOR_4D_FLAG(runon, by, ncomp, i, j, k, n,
     {
         weno_restrict_y(i,j,k,n,tmpyarr,srcarr,ratio);
+#ifdef USE_FUEGO_EOS
+        // facilitate robustness
+        Real rhomin = 1e-10;
+        Real Tmin = 1e-10;
+        tmpyarr(i,j,k,MRHO) = amrex::max(tmpyarr(i,j,k,MRHO), rhomin);
+        for (int n = 0; n < NSP; ++n) {
+            tmpyarr(i,j,k,n) = amrex::max(tmpyarr(i,j,k,n),0.0);  
+        }
+        tmpyarr(i,j,k,MT) = amrex::max(tmpyarr(i,j,k,MT), Tmin);
+#endif
     });
 #endif
 
@@ -1777,7 +1828,17 @@ CellWENO::restrict (const FArrayBox& fine,
                                            i, j, k, n,
     {
         weno_restrict_x(i,j,k,n,crsearr,srcarr,ratio);
+#ifdef USE_FUEGO_EOS
+        // facilitate robustness
+        Real rhomin = 1e-10;
+        Real Tmin = 1e-10;
+        crsearr(i,j,k,MRHO) = amrex::max(crsearr(i,j,k,MRHO), rhomin);
+        for (int n = 0; n < NSP; ++n) {
+            crsearr(i,j,k,n) = amrex::max(crsearr(i,j,k,n),0.0);  
+        }
+        crsearr(i,j,k,MT) = amrex::max(crsearr(i,j,k,MT), Tmin);
     });
+#endif
 }
 
 }
